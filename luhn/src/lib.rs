00ba1f63238d8f7fadadfb8ae1d3155abd51758b
Vincent Serpoul <vincent@serpoul.com>
2018-11-30 15:44:51 +0800

    luhn more compact

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,34 +1,34 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    // remove spaces
-    let clean_code = code.chars().filter(|c| *c != ' ').collect::<String>();
-
-    if clean_code.len() <= 1 {
-        return false;
-    }
-    if !clean_code.chars().all(char::is_numeric) {
-        return false;
-    }
-
-    if clean_code
+    let c = code
         .chars()
+        .filter(|c| !c.is_whitespace())
         .rev()
-        .enumerate()
-        .map(|(i, c)| {
-            let d = c.to_digit(10).unwrap();
-            if i % 2 == 0 {
-                return d;
-            }
-            if (d * 2) > 9 {
-                return (d * 2) - 9;
-            }
-            (d * 2)
-        }).sum::<u32>()
-        % 10
-        != 0
-    {
+        .collect::<Vec<char>>();
+
+    if c.len() <= 1 {
         return false;
     }
 
-    true
+    match c
+        .iter()
+        .enumerate()
+        .try_fold(0, |acc, (i, c)| match c.to_digit(10) {
+            None => Err("not a number"),
+            Some(d) => Ok(add_digit(acc, i, d)),
+        }) {
+        Err(_) => return false,
+        Ok(sum) => (sum % 10) == 0,
+    }
+}
+
+// add_digit add a digit d to the accumulator acc, according to index i
+fn add_digit(acc: u32, i: usize, d: u32) -> u32 {
+    if i % 2 == 0 {
+        return acc + d;
+    }
+    if (d * 2) > 9 {
+        return acc + (d * 2) - 9;
+    }
+    acc + d * 2
 }
