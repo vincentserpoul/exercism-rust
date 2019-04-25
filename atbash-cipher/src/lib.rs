2bb570fad630c267406be905a3ec6787754e9521
Vincent Serpoul <vincent@serpoul.com>
2019-04-25 11:08:27 +0800

    update with mentor feedback

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,19 +1,19 @@
+use std::iter::FromIterator;
+
 const ALPH: &str = "abcdefghijklmnopqrstuvwxyz";
 
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
         plain.to_lowercase()
                 .chars()
-                .filter(|c| c.is_ascii_alphanumeric())
+                .filter(char::is_ascii_alphanumeric)
+                .map(|c| match c {
+                        c if c.is_numeric() => c,
+                        _ => ALPH.chars().rev().nth(ALPH.find(c).unwrap()).unwrap(),
+                })
                 .collect::<Vec<char>>()
                 .chunks(5)
-                .map(|w| {
-                        w.iter().filter_map(|c| match c {
-                                c if c.is_numeric() => Some(*c),
-                                _ => ALPH.chars().rev().nth(ALPH.find(*c).unwrap()),
-                        })
-                        .collect::<String>()
-                })
+                .map(String::from_iter)
                 .collect::<Vec<String>>()
                 .join(" ")
 }
@@ -21,15 +21,10 @@ pub fn encode(plain: &str) -> String {
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
         cipher.chars()
-                .filter(|c| c.is_alphanumeric())
-                .filter_map(|c| {
-                        if c.is_numeric() {
-                                return Some(c);
-                        }
-                        match ALPH.find(c) {
-                                None => None,
-                                Some(idx) => ALPH.chars().rev().nth(idx),
-                        }
+                .filter(char::is_ascii_alphanumeric)
+                .map(|c| match c {
+                        c if c.is_numeric() => c,
+                        _ => ALPH.chars().rev().nth(ALPH.find(c).unwrap()).unwrap(),
                 })
                 .collect::<String>()
 }
