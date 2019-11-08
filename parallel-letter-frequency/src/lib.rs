4c8b1fa6d82d581cb51c3e92e9f15c08968d801a
Vincent Serpoul <vincent@serpoul.com>
2019-11-08 17:09:38 +0800

    working

---
+++ parallel-letter-frequency/src/lib.rs
@@ -0,0 +1,29 @@
+use std::collections::HashMap;
+extern crate rayon;
+use rayon::prelude::*;
+
+pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(worker_count)
+        .build()
+        .unwrap();
+
+    input
+        .par_iter()
+        .map(|sentence| {
+            sentence
+                .to_lowercase()
+                .chars()
+                .filter(|c| c.is_alphabetic())
+                .fold(HashMap::<char, usize>::new(), |mut hm, ch| {
+                    *hm.entry(ch).or_insert(0) += 1;
+                    hm
+                })
+        })
+        .reduce(HashMap::<char, usize>::new, |mut hm, sentence_hm| {
+            for (letter, count) in sentence_hm {
+                *hm.entry(letter).or_insert(0) += count;
+            }
+            hm
+        })
+}
